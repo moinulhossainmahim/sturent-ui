36561e2f83b49f93bbc39871864536293e374d8e
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-14 00:12:13 +0600

    refactor: gender UI implementation refactored

--- components/shared/inputs/Gender.tsx
+++ components/shared/inputs/Gender.tsx
@@ -3,13 +3,31 @@ interface GenderProps {
   setGender: Function;
 }
 
+const genderList = [
+  {
+    id: 1,
+    name: 'Male',
+  },
+  {
+    id: 2,
+    name: 'Female',
+  },
+  {
+    id: 3,
+    name: 'Both',
+  },
+]
+
 const Gender = ({ gender, setGender } : GenderProps) => {
   return (
     <div className="w-full flex justify-center">
       <ul className="w-[80%] flex flex-col gap-2 text-center">
-        <li className={`cursor-pointer py-4 w-full bg-gray-50 font-medium rounded-md hover:bg-gray-100 ${gender === 'Male' ? 'bg-gray-200' : ''}`} onClick={() => setGender('Male')}>Male</li>
-        <li className={`cursor-pointer py-4 w-full bg-gray-50 font-medium rounded-md hover:bg-gray-100 ${gender === 'Female' ? 'bg-gray-200' : ''}`} onClick={() => setGender('Female')}>Female</li>
-        <li className={`cursor-pointer py-4 w-full bg-gray-50 font-medium rounded-md hover:bg-gray-100 ${gender === 'Both' ? 'bg-gray-200' : ''}`} onClick={() => setGender('Both')}>Both</li>
+        {genderList.map((item) => (
+          <li
+            className={`cursor-pointer py-4 w-full bg-gray-50 font-medium rounded-md hover:bg-gray-100 ${gender === item.name ? 'bg-gray-200' : ''}`}
+            onClick={() => setGender(item.name)}
+          >{item.name}</li>
+        ))}
       </ul>
     </div>
   )
