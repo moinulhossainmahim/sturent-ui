de23e51a97b3f9309f43ebd2423caff682723502
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-13 19:50:30 +0600

    feat: added search modal

---
+++ components/shared/modals/SearchModal.tsx
@@ -0,0 +1,142 @@
+'use client';
+
+import qs from 'query-string';
+import { useCallback, useMemo, useState } from "react";
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import useSearchModal from "@/app/hooks/useSearchModal";
+
+import Modal from "./Modal";
+import Gender from '../inputs/Gender';
+import Institution from '../inputs/Institution';
+import Place from '../inputs/Place';
+import Heading from '../Heading';
+
+enum STEPS {
+  GENDER = 0,
+  INSTITUTION = 1,
+  LOCATION = 2,
+}
+
+const SearchModal = () => {
+  const router = useRouter();
+  const searchModal = useSearchModal();
+  const params = useSearchParams();
+
+  const [step, setStep] = useState(STEPS.GENDER);
+
+  const [location, setLocation] = useState('');
+  const [institution, setInstitution] = useState('');
+  const [gender, setGender] = useState('');
+
+  const onBack = useCallback(() => {
+    setStep((value) => value - 1);
+  }, []);
+
+  const onNext = useCallback(() => {
+    setStep((value) => value + 1);
+  }, []);
+
+  const onSubmit = useCallback(async () => {
+    if (step !== STEPS.LOCATION) {
+      return onNext();
+    }
+
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      locationValue: location,
+      institution,
+      gender,
+    };
+
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery,
+    }, { skipNull: true });
+
+    setStep(STEPS.LOCATION);
+    searchModal.onClose();
+    router.push(url);
+  },
+  [
+    step,
+    searchModal,
+    location,
+    router,
+    onNext,
+    params,
+    institution,
+    gender,
+  ]);
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.LOCATION) {
+      return 'Search'
+    }
+
+    return 'Next'
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.GENDER) {
+      return undefined
+    }
+
+    return 'Back'
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="For which gender you want the room?"
+        subtitle="Find the perfect location!"
+      />
+      <Gender />
+    </div>
+  )
+
+  if (step === STEPS.INSTITUTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="What Institution nears to you?"
+          subtitle="Make sure everyone is free!"
+        />
+        <Institution />
+      </div>
+    )
+  }
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="More information"
+          subtitle="Find your perfect place!"
+        />
+        <Place />
+      </div>
+    )
+  }
+
+  return (
+    <Modal
+      isOpen={searchModal.isOpen}
+      title="Filters"
+      actionLabel={actionLabel}
+      onSubmit={onSubmit}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.GENDER ? undefined : onBack}
+      onClose={searchModal.onClose}
+      body={bodyContent}
+    />
+  );
+}
+
+export default SearchModal;
