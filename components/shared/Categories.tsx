de07e7ed31f5e5e1f8f766cd6f114d997e5a6469
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-13 18:00:21 +0600

    refactor: updated category list items

--- components/shared/Categories.tsx
+++ components/shared/Categories.tsx
@@ -1,17 +1,9 @@
 'use client';
 
 import { usePathname, useSearchParams } from 'next/navigation';
-import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
-import {
-  GiBoatFishing,
-  GiCastle,
-  GiCaveEntrance,
-  GiForestCamp,
-  GiIsland,
-  GiWindmill
-} from 'react-icons/gi';
-import { FaSkiing } from 'react-icons/fa';
-import { MdOutlineVilla } from 'react-icons/md';
+import { FaSlideshare } from 'react-icons/fa';
+import { MdBedroomChild } from 'react-icons/md';
+import { IoHome } from "react-icons/io5";
 
 import CategoryBox from './CategoryBox';
 import Container from './Container';
@@ -19,59 +11,19 @@ import { ICategory } from '@/types';
 
 export const categories: ICategory[] = [
   {
-    label: 'Beach',
-    icon: TbBeach,
-    description: 'This property is close to the beach!',
+    label: 'Single',
+    icon: MdBedroomChild,
+    description: 'This category contains single room',
   },
   {
-    label: 'Windmills',
-    icon: GiWindmill,
-    description: 'This property is has windmills!',
+    label: 'Sublet',
+    icon: FaSlideshare,
+    description: 'This category contains sublet rooms',
   },
   {
-    label: 'Modern',
-    icon: MdOutlineVilla,
-    description: 'This property is modern!'
-  },
-  {
-    label: 'Countryside',
-    icon: TbMountain,
-    description: 'This property is in the countryside!'
-  },
-  {
-    label: 'Pools',
-    icon: TbPool,
-    description: 'This is property has a beautiful pool!'
-  },
-  {
-    label: 'Islands',
-    icon: GiIsland,
-    description: 'This property is on an island!'
-  },
-  {
-    label: 'Lake',
-    icon: GiBoatFishing,
-    description: 'This property is near a lake!'
-  },
-  {
-    label: 'Skiing',
-    icon: FaSkiing,
-    description: 'This property has skiing activies!'
-  },
-  {
-    label: 'Castles',
-    icon: GiCastle,
-    description: 'This property is an ancient castle!'
-  },
-  {
-    label: 'Caves',
-    icon: GiCaveEntrance,
-    description: 'This property is in a spooky cave!'
-  },
-  {
-    label: 'Camping',
-    icon: GiForestCamp,
-    description: 'This property offers camping activities!'
+    label: 'Flat',
+    icon: IoHome,
+    description: 'This property is for flats'
   },
 ]
 
