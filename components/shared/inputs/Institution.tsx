16d610c6e0b79160e98ca4aac7d9996a54250fb6
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-13 23:53:50 +0600

    feat: updated institution UI with select option

--- components/shared/inputs/Institution.tsx
+++ components/shared/inputs/Institution.tsx
@@ -1,7 +1,47 @@
-const Institution = () => {
+interface InstitutionProps {
+  institution: string;
+  setInstitution: Function;
+}
+
+const institutionList = [
+  {
+    id: 1,
+    name: 'Brac University',
+  },
+  {
+    id: 2,
+    name: 'North South University',
+  },
+  {
+    id: 3,
+    name: 'Northern University',
+  },
+  {
+    id: 4,
+    name: 'American International University',
+  },
+  {
+    id: 5,
+    name: 'Independent University',
+  },
+]
+
+const Institution = ({ institution, setInstitution } : InstitutionProps) => {
   return (
-    <div>Institution</div>
+    <div className="w-full flex justify-center">
+      <ul className="w-[80%] flex flex-col gap-2 text-center max-h-[350px] overflow-y-auto">
+        {institutionList.map((institute) => (
+          <li
+            key={institute.id}
+            className={
+              `cursor-pointer py-4 w-full bg-gray-50 font-medium rounded-md hover:bg-gray-100 ${institute.name === institution ? 'bg-gray-200' : ''}`
+            }
+            onClick={() => setInstitution(institute.name)}
+          >{institute.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default Institution
+export default Institution;
