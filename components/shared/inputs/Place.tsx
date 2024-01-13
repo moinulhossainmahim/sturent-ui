5275945c43bd40ec106f094f6d6bcc55d1a979cc
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-13 23:54:17 +0600

    feat: Implemented place UI

--- components/shared/inputs/Place.tsx
+++ components/shared/inputs/Place.tsx
@@ -1,7 +1,47 @@
-const Place = () => {
+interface PlaceProps {
+  place: string;
+  setplace: Function;
+}
+
+const placeList = [
+  {
+    id: 1,
+    name: 'Uttara, Dhaka',
+  },
+  {
+    id: 2,
+    name: 'Badda, Dhaka',
+  },
+  {
+    id: 3,
+    name: 'Banani, Dhaka',
+  },
+  {
+    id: 4,
+    name: 'Gulshan, Dhaka',
+  },
+  {
+    id: 5,
+    name: 'Dhanmodi, Dhaka',
+  },
+]
+
+const Place = ({ place, setplace } : PlaceProps) => {
   return (
-    <div>Place</div>
+    <div className="w-full flex justify-center">
+      <ul className="w-[80%] flex flex-col gap-2 text-center max-h-[350px] overflow-y-auto">
+        {placeList.map((item) => (
+          <li
+            key={item.id}
+            className={
+              `cursor-pointer py-4 w-full bg-gray-50 font-medium rounded-md hover:bg-gray-100 ${item.name === place ? 'bg-gray-200' : ''}`
+            }
+            onClick={() => setplace(item.name)}
+          >{item.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default Place
+export default Place;
