ca94d0d08c8e41e6fc15576aa773691e7957b5de
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-13 18:44:56 +0600

    chore: updated container managing category width

--- components/shared/Container.tsx
+++ components/shared/Container.tsx
@@ -1,18 +1,21 @@
 interface ContainerProps {
   children: React.ReactNode;
+  isCategory?: boolean;
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
+const Container: React.FC<ContainerProps> = ({ children, isCategory }) => {
   return (
     <div
-      className="
-        max-w-[2520px]
-        mx-auto
-        xl:px-20
-        md:px-10
-        sm:px-2
-        px-4
-      "
+      className={
+      `max-w-[2520px]
+      mx-auto
+      xl:px-20
+      md:px-10
+      sm:px-2
+      px-4
+      ${isCategory ? 'flex justify-center' : ''}
+      `
+    }
     >
       {children}
     </div>
