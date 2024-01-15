a653d13a266465ff12eaaa2b4ed911cfbdf8c864
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-16 02:49:49 +0600

    feat: added features UI

--- components/shared/listings/ListingInfo.tsx
+++ components/shared/listings/ListingInfo.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import { IconType } from "react-icons";
-import { FaBed, FaBath } from "react-icons/fa6";
-import { MdCameraRear, MdAllInbox } from "react-icons/md";
-
-import Avatar from "../Avatar";
-import ListingCategory from "./ListingCategory";
+import { FaBed, FaBath, FaKitchenSet, FaCar } from "react-icons/fa6";
+import { MdAllInbox, MdOutlineBalcony, MdElevator } from "react-icons/md";
+import { BiSolidCctv } from "react-icons/bi";
+import { Button } from "@/components/ui/button";
 
 interface ListingInfoProps {
   description?: string;
@@ -21,6 +20,34 @@ interface ListingInfoProps {
   img: string;
 }
 
+const features = [
+  {
+    id: 1,
+    icon: MdOutlineBalcony,
+    name: 'Balcony',
+  },
+  {
+    id: 2,
+    icon: FaCar,
+    name: 'Free Parking',
+  },
+  {
+    id: 3,
+    icon: FaKitchenSet,
+    name: 'Kitchen',
+  },
+  {
+    id: 4,
+    icon: MdElevator,
+    name: 'Elevator',
+  },
+  {
+    id: 5,
+    icon: BiSolidCctv,
+    name: 'CCTV Security',
+  },
+]
+
 const ListingInfo: React.FC<ListingInfoProps> = ({
   description = '',
   guestCount = 1,
@@ -31,23 +58,75 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
 }) => {
 
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-5">
       <div className="flex flex-col gap-4 w-3/4">
         <h1 className="text-2xl font-bold">BDT 80 Thousand</h1>
         <h4 className="text-md font-medium">Sector 3, Uttara, Dhaka</h4>
         <div className="flex gap-7">
           <div className="flex gap-1 items-center">
             <FaBed size={20} />
-            <span>3 Beds</span>
+            <span className="font-sm">3 Beds</span>
           </div>
           <div className="flex gap-1 items-center">
             <FaBath size={20} />
-            <span>4 Baths</span>
+            <span className="font-sm">4 Baths</span>
           </div>
           <div className="flex gap-1 items-center">
             <MdAllInbox size={20} />
-            <span>2,180 sqft</span>
+            <span className="font-sm">2,180 sqft</span>
           </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-lg font-semibold my-4">A Contemporary Cozy Flat Of 2180 Sq Ft Is Up For Rent Is Situated In Sector 03, Uttara</h1>
+          <p className="leading-7">
+            Find your home in this 2180  Square Feet apartment in Uttara.
+            Featuring 3 beds and 4 baths, this cozy and beautiful apartment can be a perfect fit for families who are planning to make a home in Uttara.
+            Just as you enter the apartment, you will find open space for drawing and dining space. The master bedroom has attached balconies with it.
+            A beautiful kitchen with durable fittings is there to make your cooking time comforting. The security service and maintenance of this building
+            is top notch that ensures a secure and healthy life for your family.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-lg font-semibold my-6">General Information</h1>
+          <div className="flex justify-between">
+            <div className="flex flex-col gap-4 w-[45%]">
+              <div className="flex gap-4 items-center">
+                <p>Type</p>
+                <h5 className="text-lg font-semibold">Room</h5>
+              </div>
+              <hr />
+              <div className="flex gap-4 items-center">
+                <p>Purpose</p>
+                <h5 className="text-lg font-semibold">For Rent</h5>
+              </div>
+              <hr />
+            </div>
+            <div className="flex flex-col gap-4 w-[45%]">
+              <div className="flex gap-4 items-center">
+                <p>Tracking no.</p>
+                <h5 className="text-lg font-semibold">TN2022234321</h5>
+              </div>
+              <hr />
+              <div className="flex gap-4 items-center">
+                <p>Last Updated</p>
+                <h5 className="text-lg font-semibold">11 January 2024</h5>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+        <div className="py-4">
+          <h1 className="text-lg font-semibold my-6">What this Room offers</h1>
+          <div className="flex gap-6 mb-6 flex-wrap">
+            {features.map((feature) => (
+              <div className="h-[8rem] w-[10rem] flex flex-col gap-2 justify-center items-center bg-accent rounded-xl" key={feature.id}>
+                {<feature.icon size={25} />}
+                <p>{feature.name}</p>
+              </div>
+            ))}
+          </div>
+          <Button className="mb-4" variant='outline' size='lg'>Show All Facilities</Button>
+          <hr />
         </div>
       </div>
       <div className="w-1/4">
