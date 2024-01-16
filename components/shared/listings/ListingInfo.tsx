3facfdd7eab808b56469c9ee94257b5db88a5b65
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-16 11:56:30 +0600

    feat: added recommended listing in listing details page

--- components/shared/listings/ListingInfo.tsx
+++ components/shared/listings/ListingInfo.tsx
@@ -2,10 +2,11 @@
 
 import { IconType } from "react-icons";
 import { FaBed, FaBath, FaKitchenSet, FaCar, FaWifi } from "react-icons/fa6";
-import { MdAllInbox, MdOutlineBalcony, MdElevator, MdOutlineTableBar } from "react-icons/md";
+import { MdAllInbox, MdOutlineBalcony, MdElevator } from "react-icons/md";
 import { BiSolidCctv } from "react-icons/bi";
 import { Button } from "@/components/ui/button";
 import useListingFeaturesModal from "@/app/hooks/useListingFeaturesModal";
+import ListingCard from "./ListingCard";
 
 interface ListingInfoProps {
   description?: string;
@@ -21,7 +22,49 @@ interface ListingInfoProps {
   img: string;
 }
 
-const features = [
+const newListings = [
+  {
+    id: 1,
+    title: "Cozy Apartment in the City Center",
+    description: "A comfortable and stylish apartment located in the heart of the city.",
+    featuredImg: "https://source.unsplash.com/800x600/?apartment",
+    galleryImg: ["https://source.unsplash.com/800x600/?living-room"],
+    roomCount: 2,
+    bathroomCount: 1,
+    guestCount: 4,
+    price: 15,
+    location: 'Uttara, Dhaka',
+    isSaved: false,
+  },
+  {
+    id: 2,
+    title: "Spacious Family Home with Garden",
+    description: "A large and welcoming family home with a beautiful garden for relaxation.",
+    featuredImg: "https://source.unsplash.com/800x600/?house",
+    galleryImg: ["https://source.unsplash.com/800x600/?family-room"],
+    roomCount: 4,
+    bathroomCount: 2,
+    guestCount: 8,
+    price: 23,
+    location: 'Mirpur 10, Dhaka',
+    isSaved: false,
+  },
+  {
+    id: 3,
+    title: "Downtown Loft with City View",
+    description: "A trendy loft with stunning views of the city skyline.",
+    featuredImg: "https://source.unsplash.com/800x600/?loft",
+    galleryImg: ["https://source.unsplash.com/800x600/?city-view"],
+    roomCount: 1,
+    bathroomCount: 1,
+    guestCount: 2,
+    price: 10,
+    location: 'Chakbazar, Chattogram',
+    isSaved: false,
+  },
+]
+
+export const features = [
   {
     id: 1,
     icon: MdOutlineBalcony,
@@ -134,6 +177,27 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
           </div>
           <Button className="mb-4 border-neutral-900 border-2" variant='outline' size='lg' onClick={featuresModal.onOpen}>Show All Facilities</Button>
           <hr />
+        </div>
+        <div>
+          <h1 className="text-lg font-semibold my-6">Recommended Rooms</h1>
+          <div className="
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            2xl:grid-cols-5
+            gap-8
+            mb-8
+            "
+          >
+            {newListings.map((listing) => (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+              />
+            ))}
+          </div>
         </div>
       </div>
       <div className="w-1/4">
