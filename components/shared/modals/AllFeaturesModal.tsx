93646a18a8bf09980048d80df1beca488b587ee1
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-16 11:55:15 +0600

    feat: implemented all features UI modal

--- components/shared/modals/AllFeaturesModal.tsx
+++ components/shared/modals/AllFeaturesModal.tsx
@@ -2,19 +2,31 @@
 
 import useListingFeaturesModal from "@/app/hooks/useListingFeaturesModal";
 import Modal from "./Modal";
+import { features } from "../listings/ListingInfo";
 
 const AllFeaturesModal = () => {
   const featuresModal = useListingFeaturesModal();
 
   const bodyContent = (
-    <h1>Hello world!</h1>
+    <div>
+      <h1 className="text-lg font-semibold my-4">General Facilities</h1>
+      <div className="flex gap-6 mb-6 flex-wrap">
+        {features.map((feature) => (
+          <div className="h-[8rem] w-[10rem] flex flex-col gap-2 justify-center items-center bg-accent rounded-xl" key={feature.id}>
+            {<feature.icon size={25} />}
+            <p>{feature.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 
   return (
     <Modal
       isOpen={featuresModal.isOpen}
       onClose={featuresModal.onClose}
-      actionLabel='What this place offers'
+      title="What this place offers"
+      actionLabel=""
       onSubmit={() => {}}
       body={bodyContent}
     />
