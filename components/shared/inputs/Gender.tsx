f279adbfcf01c0b04727f673949f9dd2eb082d40
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-22 18:15:58 +0600

    fix: fixed key prop issue

--- components/shared/inputs/Gender.tsx
+++ components/shared/inputs/Gender.tsx
@@ -24,6 +24,7 @@ const Gender = ({ gender, setGender } : GenderProps) => {
       <ul className="w-[80%] flex flex-col gap-2 text-center">
         {genderList.map((item) => (
           <li
+            key={item.id}
             className={`cursor-pointer py-4 w-full bg-muted font-medium rounded-md ${gender === item.name ? 'bg-gray-200' : ''}`}
             onClick={() => setGender(item.name)}
           >{item.name}</li>
