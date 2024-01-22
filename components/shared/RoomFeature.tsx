f6008acb7eaa88c52d48f216554cd9c9529674bb
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-22 18:16:27 +0600

    feat: added room feature component

---
+++ components/shared/RoomFeature.tsx
@@ -0,0 +1,35 @@
+import { Feature } from "@/types/Feature";
+
+interface RoomFeatureProps {
+  feature: Feature;
+  toggleFeature: (featureId: number) => void;
+  selectedFeatures?: number[]
+}
+
+const RoomFeature = ({ feature, toggleFeature, selectedFeatures } : RoomFeatureProps) => {
+  return (
+    <div
+      className={`
+      min-w-[10rem]
+      h-[8rem]
+      flex
+      flex-col
+      gap-2
+      justify-center
+      items-center
+      bg-accent
+      rounded-xl
+      border-2
+      hover:border-foreground
+      ${selectedFeatures ? 'cursor-pointer' : ''}
+      ${selectedFeatures?.includes(feature?.id) ? 'border-foreground' : 'border-muted'}
+      `}
+      onClick={() => toggleFeature(feature.id)}
+    >
+      {<feature.icon size={25} />}
+      <p>{feature.name}</p>
+    </div>
+  )
+}
+
+export default RoomFeature;
