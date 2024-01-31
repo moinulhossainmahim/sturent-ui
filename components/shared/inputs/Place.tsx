58eb57e422523a74dbb37fcb8e09eacd87eb31d5
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-31 22:58:10 +0600

    refactor: updated place search UI

--- components/shared/inputs/Place.tsx
+++ components/shared/inputs/Place.tsx
@@ -1,3 +1,5 @@
+import CategoryInput from "./CategoryInput";
+
 interface PlaceProps {
   place: string;
   setplace: Function;
@@ -29,17 +31,15 @@ const placeList = [
 const Place = ({ place, setplace } : PlaceProps) => {
   return (
     <div className="w-full flex justify-center">
-      <ul className="w-[80%] flex flex-col gap-2 text-center max-h-[350px] overflow-y-auto">
+      <div className="w-[60%] flex flex-col gap-2 text-center max-h-[350px] overflow-y-auto">
         {placeList.map((item) => (
-          <li
-            key={item.id}
-            className={
-              `cursor-pointer py-4 w-full bg-muted font-medium rounded-md ${item.name === place ? 'bg-gray-200' : ''}`
-            }
+          <CategoryInput
+            label={item.name}
             onClick={() => setplace(item.name)}
-          >{item.name}</li>
+            selected={item.name === place}
+          />
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
