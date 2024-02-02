60f0aa3fb54ef5aac1b3c7f062c9a52f05771aa2
mhossain2 <moinulhossainmahim@gmail.com>
2024-02-02 16:18:58 +0600

    feat: added listings feature

--- redux/store.ts
+++ redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import modalReducer, { ModalStore } from './reducers/modal';
 import { pokemonApi } from './services/pokemon';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { listingsApi } from './services/listings';
 
 export interface ReduxStore {
   modal: ModalStore;
@@ -11,9 +12,10 @@ const store = configureStore({
   reducer: {
     modal: modalReducer,
     [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [listingsApi.reducerPath]: listingsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat([pokemonApi.middleware, listingsApi.middleware]),
 })
 
 setupListeners(store.dispatch);
