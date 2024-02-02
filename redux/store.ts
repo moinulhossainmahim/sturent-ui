0d0e6849d5be6e8caec10b5656f8caee678fce15
mhossain2 <moinulhossainmahim@gmail.com>
2024-02-02 15:21:05 +0600

    feat: added test pokemon api service

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modalReducer, { ModalStore } from './reducers/modal';
+import { pokemonApi } from './services/pokemon';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export interface ReduxStore {
   modal: ModalStore;
@@ -8,7 +10,12 @@ export interface ReduxStore {
 const store = configureStore({
   reducer: {
     modal: modalReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 })
+
+setupListeners(store.dispatch);
 
 export default store;
