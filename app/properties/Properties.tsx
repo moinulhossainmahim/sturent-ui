cdd2b123fbdffbc2c9190cd39296826f0f7d62c5
mhossain2 <moinulhossainmahim@gmail.com>
2024-02-02 15:19:54 +0600

    style: refactor properties class names

--- app/properties/Properties.tsx
+++ app/properties/Properties.tsx
@@ -13,19 +13,7 @@ const Properties = () => {
           title="Properties"
           subtitle="List of your properties"
           />
-        <div
-          className="
-          mt-10
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-          "
-          >
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {properties.map((listing: any) => (
             <ListingCard
               key={listing.id}
