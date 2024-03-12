6a0e4a6b488d73df81721e991b719328314243aa
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-13 05:22:31 +0600

    feat: added api slice for auth

---
+++ redux/features/auth/authApiSlice.ts
@@ -0,0 +1,17 @@
+import { apiSlice } from "../api";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: credentials => ({
+                url: '/auth/local/signin',
+                method: 'POST',
+                body: { ...credentials }
+            })
+        }),
+    })
+})
+
+export const {
+    useLoginMutation
+} = authApiSlice;
