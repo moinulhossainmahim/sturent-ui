4481df391a185ef59c1bdfa83692e522dbc3437d
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-13 05:26:22 +0600

    feat: added auth slice and reducer to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,21 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
-import modalReducer, { ModalStore } from './reducers/modal';
+import modalReducer, { ModalStore } from './features/modals/modalSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { listingsApi } from './services/listings';
-import { propertiesApi } from './services/properties';
+import authReducer, { AuthStore } from './features/auth/authSlice';
+import { authApiSlice } from './features/auth/authApiSlice';
+import { listingsApi } from './features/listings';
 
 export interface ReduxStore {
   modal: ModalStore;
+  auth: AuthStore,
 };
 
 const store = configureStore({
   reducer: {
     modal: modalReducer,
+    auth: authReducer,
     [listingsApi.reducerPath]: listingsApi.reducer,
-    [propertiesApi.reducerPath]: propertiesApi.reducer,
+    // [propertiesApi.reducerPath]: propertiesApi.reducer,
+    [authApiSlice.reducerPath]: authApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([listingsApi.middleware]),
+    getDefaultMiddleware().concat([authApiSlice.middleware, listingsApi.middleware]),
+  devTools: true,
 })
 
 setupListeners(store.dispatch);
