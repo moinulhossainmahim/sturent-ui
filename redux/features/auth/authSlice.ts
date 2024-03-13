a64784d409e612b4f388480a5b8d6d9a597840a6
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-14 05:59:57 +0600

    feat: added is authenticated property

--- redux/features/auth/authSlice.ts
+++ redux/features/auth/authSlice.ts
@@ -10,29 +10,33 @@ export interface AuthStore {
   user: IUser;
   accessToken: string;
   refreshToken: string;
+  isAuthenticated: boolean;
 };
 
 const initialState: Partial<AuthStore> = {
   user: undefined,
   accessToken: '',
   refreshToken: '',
+  isAuthenticated: false,
 };
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        setCredentials: (state, action: PayloadAction<AuthStore>) => {
-            const { user, accessToken, refreshToken } = action.payload
-            state.user = user;
-            state.accessToken = accessToken;
-            state.refreshToken = refreshToken;
-        },
-        logOut: (state) => {
-            state.user = undefined;
-            state.accessToken = '';
-            state.refreshToken = '';
-        }
+      setCredentials: (state, action: PayloadAction<AuthStore>) => {
+          const { user, accessToken, refreshToken } = action.payload
+          state.user = user;
+          state.accessToken = accessToken;
+          state.refreshToken = refreshToken;
+          state.isAuthenticated = true;
+      },
+      logOut: (state) => {
+          state.user = undefined;
+          state.accessToken = '';
+          state.refreshToken = '';
+          state.isAuthenticated = false;
+      }
     },
 })
 
