05448d16193de2700846d5e1c9ad09092760c808
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-14 05:57:05 +0600

    feat: added logout mutation

--- redux/features/auth/authApiSlice.ts
+++ redux/features/auth/authApiSlice.ts
@@ -1,17 +1,24 @@
 import { apiSlice } from "../api";
 
 export const authApiSlice = apiSlice.injectEndpoints({
-    endpoints: builder => ({
-        login: builder.mutation({
-            query: credentials => ({
-                url: '/auth/local/signin',
-                method: 'POST',
-                body: { ...credentials }
-            })
-        }),
-    })
+  endpoints: builder => ({
+      login: builder.mutation({
+        query: credentials => ({
+            url: '/auth/local/signin',
+            method: 'POST',
+            body: { ...credentials }
+        })
+      }),
+      logout: builder.mutation({
+        query: () => ({
+            url: '/auth/logout',
+            method: 'POST',
+        })
+      }),
+  })
 })
 
 export const {
-    useLoginMutation
+    useLoginMutation,
+    useLogoutMutation,
 } = authApiSlice;
