bae294d080cb9815c0c5436c869c5a3364b3d264
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-14 06:27:55 +0600

    chore: commented fetch listing api

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,19 @@ import { useGetListingsQuery } from "@/redux/features/listings";
 import { listings, testListings } from "@/test-data/listings";
 
 export default function Home() {
-  const { isLoading, data, isError } = useGetListingsQuery();
+  // const { isLoading, data, isError } = useGetListingsQuery();
 
-  if (isLoading) {
-    return (
-      <Container>
-        <div className="pt-[2rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {testListings.map((listing) => (
-            <SkeletonLoader key={listing} />
-          ))}
-        </div>
-    </Container>
-    )
-  }
+  // if (isLoading) {
+  //   return (
+  //     <Container>
+  //       <div className="pt-[2rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+  //         {testListings.map((listing) => (
+  //           <SkeletonLoader key={listing} />
+  //         ))}
+  //       </div>
+  //   </Container>
+  //   )
+  // }
 
   return (
     <Container>
