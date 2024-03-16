248ffc7d246e465be70298c3ab2e144e6c8240da
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-17 04:04:20 +0600

    chore: updated redux store with necessary updates

--- redux/store.ts
+++ redux/store.ts
@@ -2,9 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import modalReducer, { ModalStore } from './features/modals/modalSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import authReducer, { AuthStore } from './features/auth/authSlice';
-import { authApiSlice } from './features/auth/authApiSlice';
-import { listingsApi } from './features/listings';
-import { propertiesApiSlice } from './features/properties/propertiesApiSlice';
+import { apiSlice } from './features/api';
 
 export interface ReduxStore {
   modal: ModalStore;
@@ -15,18 +13,10 @@ const store = configureStore({
   reducer: {
     modal: modalReducer,
     auth: authReducer,
-    [propertiesApiSlice.reducerPath]: propertiesApiSlice.reducer,
-    [listingsApi.reducerPath]: listingsApi.reducer,
-    [authApiSlice.reducerPath]: authApiSlice.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      [
-        authApiSlice.middleware,
-        propertiesApiSlice.middleware,
-        listingsApi.middleware,
-      ]
-    ),
+    getDefaultMiddleware().concat([apiSlice.middleware]),
   devTools: true,
 })
 
