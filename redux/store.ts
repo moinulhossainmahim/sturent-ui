e9b694911643ad3f0f5e7351e8af8a8625d634ba
mhossain2 <moinulhossainmahim@gmail.com>
2024-03-17 15:39:01 +0600

    feat: added all newly added slice to the redux store

--- redux/store.ts
+++ redux/store.ts
@@ -3,16 +3,28 @@ import modalReducer, { ModalStore } from './features/modals/modalSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import authReducer, { AuthStore } from './features/auth/authSlice';
 import { apiSlice } from './features/api';
+import propertyReducer, { PropertyStore } from './features/properties/propertiesSlice';
+import userPropertyReducer, { UserPropertiesStore } from './features/properties/userPropertiesSlice';
+import favoriteReducer, { FavoriteStore } from './features/favorites/favoritesSlice';
+import singlePropertyReducer, { SinglePropertyStore } from './features/properties/singlePropetySlice';
 
 export interface ReduxStore {
   modal: ModalStore;
-  auth: AuthStore,
+  auth: AuthStore;
+  properties: PropertyStore;
+  userProperties: UserPropertiesStore;
+  favorites: FavoriteStore;
+  singlePropertyStore: SinglePropertyStore;
 };
 
 const store = configureStore({
   reducer: {
     modal: modalReducer,
     auth: authReducer,
+    properties: propertyReducer,
+    userProperties: userPropertyReducer,
+    favorites: favoriteReducer,
+    singlePropertyReducer: singlePropertyReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
