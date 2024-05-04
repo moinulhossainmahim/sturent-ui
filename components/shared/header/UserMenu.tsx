b2c7b4cccaefe6d8ee8889e0d7d7535e0162c793
mhossain2 <moinulhossainmahim@gmail.com>
2024-05-05 02:58:38 +0600

    feat: integrate logout functionality

--- components/shared/header/UserMenu.tsx
+++ components/shared/header/UserMenu.tsx
@@ -3,16 +3,21 @@
 import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AiOutlineMenu } from "react-icons/ai";
+import { toast } from "react-toastify";
 import Image from "next/image";
 import Link from "next/link";
 
 import MenuItem from "./MenuItem";
 import { ReduxStore } from "@/redux/store";
 import { ModalKey, setModal } from '@/redux/features/modals/modalSlice';
+import { useLogoutMutation } from "@/redux/features/auth/authApiSlice";
+import { logOut } from "@/redux/features/auth/authSlice";
 
 const UserMenu= () => {
   const dispatch = useDispatch();
+  const [logout] = useLogoutMutation();
   const [isOpen, setIsOpen] = useState(false);
+  const { user, isAuthenticated } = useSelector((state: ReduxStore) => state.auth);
   const registerModal = useSelector((state: ReduxStore) => state.modal.RegisterModal);
   const loginModal = useSelector((state: ReduxStore) => state.modal.LoginModal);
 
@@ -20,12 +25,27 @@ const UserMenu= () => {
     setIsOpen((value) => !value);
   }, []);
 
+  async function handleLogout() {
+    const response = await logout({}) as { data: { message: string, status: number } };
+    if (response?.data?.status === 200) {
+      dispatch(logOut());
+      toast.success('Logged Out successfully', { autoClose: 1000 });
+      toggleOpen();
+    }
+  }
+
   return (
     <>
       <div className="relative">
         <div className="flex flex-row items-center gap-3">
           <div
-            onClick={() => dispatch(setModal({ key: ModalKey.CreatePropertyModal, value: true }))}
+            onClick={() => {
+              if(isAuthenticated) {
+                dispatch(setModal({ key: ModalKey.CreatePropertyModal, value: true }))
+              } else {
+                dispatch(setModal({ key: ModalKey.LoginModal, value: true }))
+              }
+            }}
             className="
               hidden
               md:block
@@ -63,7 +83,7 @@ const UserMenu= () => {
             <div className="hidden md:block">
               <Image
                 className="rounded-full"
-                src='/assets/placeholder.jpg'
+                src={ isAuthenticated && user.picture ? user.picture : '/assets/placeholder.jpg'}
                 width={30}
                 height={30}
                 alt="avatar"
@@ -87,36 +107,47 @@ const UserMenu= () => {
             "
           >
             <div className="flex flex-col cursor-pointer">
-              <MenuItem
-                onClick={() => {
-                  dispatch(setModal({ key: ModalKey.LoginModal, value: true }))
-                  toggleOpen();
-                }}
-                label="Login"
-              />
-              <MenuItem
-                onClick={() => {
-                  dispatch(setModal({ key: ModalKey.RegisterModal, value: true }))
-                  toggleOpen();
-                }}
-                label="Sign up"
-              />
-              <Link href='/properties'>
-                <MenuItem
-                  onClick={() => {
-                    toggleOpen();
-                  }}
-                  label="My Listings"
-                />
-              </Link>
-              <Link href='/favorites'>
-                <MenuItem
-                  onClick={() => {
-                    toggleOpen();
-                  }}
-                  label="My favorites"
-                />
-              </Link>
+              {!isAuthenticated ? (
+                <>
+                  <MenuItem
+                    onClick={() => {
+                      dispatch(setModal({ key: ModalKey.LoginModal, value: true }))
+                      toggleOpen();
+                    }}
+                    label="Login"
+                  />
+                  <MenuItem
+                    onClick={() => {
+                      dispatch(setModal({ key: ModalKey.RegisterModal, value: true }))
+                      toggleOpen();
+                    }}
+                    label="Sign up"
+                  />
+                </>
+              ) : (
+                <>
+                  <Link href='/properties'>
+                    <MenuItem
+                      onClick={() => {
+                        toggleOpen();
+                      }}
+                      label="My Listings"
+                      />
+                  </Link>
+                  <Link href='/favorites'>
+                    <MenuItem
+                      onClick={() => {
+                        toggleOpen();
+                      }}
+                      label="My favorites"
+                      />
+                  </Link>
+                  <MenuItem
+                    onClick={handleLogout}
+                    label="Logout"
+                  />
+                </>
+              )}
             </div>
           </div>
         )}
