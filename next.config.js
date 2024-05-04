a01c542995369c8a6b82a8ca8c6fd11edb6eee0d
mhossain2 <moinulhossainmahim@gmail.com>
2024-05-05 02:59:22 +0600

    fix: fix google image error

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: 'via.placeholder.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
         }
       ]
   }
