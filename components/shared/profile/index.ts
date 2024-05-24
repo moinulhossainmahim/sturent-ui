1497bd1be1276fd36a62bfb96fe721e61c4dd406
mhossain2 <moinulhossainmahim@gmail.com>
2024-05-24 16:12:30 +0600

    chore: added export from index file

---
+++ components/shared/profile/index.ts
@@ -0,0 +1,5 @@
+import ProfileInfo from "./ProfileInfo";
+import ProfileUserCard from "./ProfileUserCard";
+import ProfileVerificationCard from "./ProfileVerificationCard";
+
+export { ProfileInfo, ProfileUserCard, ProfileVerificationCard };
