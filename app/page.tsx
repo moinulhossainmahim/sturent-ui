f607cb19590225c7c564e430700390ab85b88bb4
mhossain2 <moinulhossainmahim@gmail.com>
2024-05-25 20:57:15 +0600

    refactor: updated root page component by removing all code to the home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,10 @@
 import Container from "@/components/shared/Container";
-import ListingCard from "@/components/shared/listings/ListingCard";
-import { SkeletonLoader } from "@/components/shared/loader/SkeletonLoader";
-import { listings, testListings } from "@/test-data/listings";
+import Home from "@/components/shared/home";
 
-export default function Home() {
-  // const { isLoading, data, isError } = useGetListingsQuery();
-
-  // if (isLoading) {
-  //   return (
-  //     <Container>
-  //       <div className="pt-[2rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-  //         {testListings.map((listing) => (
-  //           <SkeletonLoader key={listing} />
-  //         ))}
-  //       </div>
-  //   </Container>
-  //   )
-  // }
-
+export default function page() {
   return (
     <Container>
-      <div className="pt-[2rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listings.map((listing) => (
-          <ListingCard
-            key={listing.id}
-            data={listing}
-          />
-        ))}
-      </div>
+      <Home />
     </Container>
   )
 }
