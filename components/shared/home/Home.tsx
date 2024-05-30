3adfd755b60bd5df48fef28b803f3ebc908e7fb9
mhossain2 <moinulhossainmahim@gmail.com>
2024-05-31 02:30:11 +0600

    feat: included slider in home page

--- components/shared/home/Home.tsx
+++ components/shared/home/Home.tsx
@@ -6,26 +6,30 @@ import ListingHeader from "../listings/ListingHeader";
 import ListingsGrid from "../listings/ListingsGrid";
 import ListingsList from "../listings/ListingsList";
 import Container from "@/components/shared/Container";
+import Slider from "./Slider";
 
 const Home = () => {
   const [layout, setLayout] = useState('grid');
 
   return (
-    <Container isListing>
-      <div className="flex w-full pt-[2rem] gap-12 flex-col md:flex-row md:items-start items-center">
-        <div className="w-[100%] sm:w-[80%] md:w-[35%] lg:w-[25%] p-6 h-full border-muted border rounded-lg shadow-xl md:sticky md:top-[80px]">
-          <Filters />
+    <>
+      <Slider />
+      <Container isListing>
+        <div className="flex w-full pt-[2rem] gap-12 flex-col md:flex-row md:items-start items-center">
+          <div className="w-[100%] sm:w-[80%] md:w-[35%] lg:w-[25%] p-6 h-full border-muted border rounded-lg shadow-xl md:sticky md:top-[80px]">
+            <Filters />
+          </div>
+          <div className="w-[100%] md:w-[65%] lg:w-[75%] flex flex-col">
+            <ListingHeader layout={layout} setLayout={setLayout} />
+            {layout === 'grid' ? (
+              <ListingsGrid />
+            ) : (
+              <ListingsList />
+            )}
+          </div>
         </div>
-        <div className="w-[100%] md:w-[65%] lg:w-[75%] flex flex-col">
-          <ListingHeader layout={layout} setLayout={setLayout} />
-          {layout === 'grid' ? (
-            <ListingsGrid />
-          ) : (
-            <ListingsList />
-          )}
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   )
 }
 
